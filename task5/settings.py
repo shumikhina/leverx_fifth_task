5b76feacbf0a07d299eed96ba35b8ff3df43160d
Darya Shumikhina <shumikhina.darya@gmail.com>
2021-05-30 18:59:21 +0300

    Add swagger to project

--- task5/settings.py
+++ task5/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_yasg',
 
     'authapp',
     'courses',
@@ -129,3 +130,19 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
--- task5/urls.py
+++ task5/urls.py
@@ -15,7 +15,28 @@ Including another URLconf
 """
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Task 5",
+      default_version='v1',
+      description="Task 5",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="do_not_to_contact_me@seriously.com"),
+      license=openapi.License(name="Constitution"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
+    path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
     path('auth/', include(('authapp.urls', 'authapp'), namespace='authapp')),
     path('courses/', include(('courses.urls', 'courses'), namespace='courses')),
     path('lections/', include(('lections.urls', 'lections'), namespace='lections')),
